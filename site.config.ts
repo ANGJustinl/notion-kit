95c041f2e810154b19ea7c19f7cfba2911e7694e
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2024-01-01 21:25:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About 关于我',
+       pageId: '6dbcd7378eab4404a4da99c468fac7eb'
+     },
+     {
+       title: '昂昂bot',
+       pageId: 'a1ae45200e2a44d3ad87dd80d2109375'
+     }
+   ]
 })
